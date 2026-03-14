7ce1e5e43a5b52da3fc426f3131ec2f7b97a7944
scherelu <schererludwig@gmail.com>
2026-03-14 21:27:31 +0100

    test

--- CodeInspector/app/paths.py
+++ CodeInspector/app/paths.py
@@ -1,17 +1,18 @@
 import xml.etree.ElementTree as ET
 from pathlib import Path
 
-PROJECT_ROOT = Path(__file__).resolve().parents[1] # .../CodeInspector/
+CODE_INSPECTOR = Path(__file__).resolve().parents[1] # .../CodeInspector/
+ROOT = Path(__file__).resolve().parents[2] # .../ASEE-COED/
 
-BUILD_XML_PATH = PROJECT_ROOT / "build.xml"
-CONFIG_DIR = PROJECT_ROOT / "app/app_config"
-XML_REPORT_DIR = PROJECT_ROOT / "build/test-reports"
-GRADE_REPORTS_DIR = PROJECT_ROOT / "build/grade_reports"
-RECORDS_DIR = PROJECT_ROOT / "app/records"
+BUILD_XML_PATH = CODE_INSPECTOR / "build.xml"
+CONFIG_DIR = CODE_INSPECTOR / "app/app_config"
+XML_REPORT_DIR = ROOT / "reports"
+GRADE_REPORTS_DIR = CODE_INSPECTOR / "build/grade_reports"
+RECORDS_DIR = CODE_INSPECTOR / "app/records"
 
-CS_XML_FILE = XML_REPORT_DIR / "checkstyle-result.xml"
+CS_XML_FILE = XML_REPORT_DIR / "checkstyle.xml"
 PMD_XML_FILE = XML_REPORT_DIR / "pmd.xml"
-JUNIT_RESULT_FILE = XML_REPORT_DIR / "TEST-TestCardDescription.txt"
+JUNIT_RESULT_FILE = XML_REPORT_DIR / "junit-report.txt"
 GRADING_CONFIG_FILE = CONFIG_DIR / "grading_config.json"
 RECORDS_JSON_FILE = RECORDS_DIR / "records.json"
 WEIGHTED_DATA_CSV = RECORDS_DIR / "historical/weighted_data.csv"
@@ -38,7 +39,7 @@ def get_upload_dir_path() -> Path:
             
             upload_dir_path = Path(location)
             if not upload_dir_path.is_absolute():
-                return PROJECT_ROOT / upload_dir_path
+                return CODE_INSPECTOR / upload_dir_path
             return upload_dir_path
         
     raise ValueError("build.xml is missing: 'src.dir' property")
